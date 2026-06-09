89e8507151914083a3ee1956fb58ebbba6002b73
Ömer Halis DEMİR <omerhd16@outlook.com>
2026-06-09 23:18:11 +0300

    Loading sayfaları kaldırıldı.

--- app/(routes)/urunler/loading.jsx
+++
@@ -1,9 +0,0 @@
-import { ProductsCatalogSkeleton } from "@/components/status/products-catalog-skeleton";
-
-export default function UrunlerLoadingPage() {
- return (
-  <div className="container-premium page-content-offset pb-20 md:pb-28">
-   <ProductsCatalogSkeleton />
-  </div>
- );
-}
--- app/loading.jsx
+++
@@ -1,9 +0,0 @@
-import { LoadingView } from "@/components/status/loading-view";
-import { getServerDictionary } from "@/lib/i18n/server";
-
-export default async function LoadingPage() {
- const { dictionary } = await getServerDictionary();
- const copy = dictionary.status.loading;
-
- return <LoadingView title={copy.title} description={copy.description} />;
-}
